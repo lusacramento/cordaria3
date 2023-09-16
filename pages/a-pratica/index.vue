2266a51782eb1d481b0ae425f31ebfc4e4b4aa0e
lusacramento <lucianossacramento@yahoo.com.br>
2023-09-16 13:48:30 -0300

    implements the practice page

--- pages/a-pratica/index.vue
+++ pages/a-pratica/index.vue
@@ -1,15 +1,36 @@
 <template>
-  <div>
-    
-  </div>
+	<div id="the-pratice" class="cordaria">
+		<CordariaMenu />
+		<div class="exercise-nav container-fluid">
+			<div class="row text-center layer-top align-items-center">
+				<div class="col-12 col-lg-4">
+					<h1 class="title mt-3 mb-4">{{ title }}</h1>
+					<!-- :visible="isEnabledMenu" -->
+					<div class="col-12 col-lg-10">
+						<div
+							class="exercises-nav-layer d-flex flex-column align-items-center"
+						></div>
+					</div>
+				</div>
+			</div>
+			<div class="row justify-content-center bg-exercise-screen">
+				<div class="col-lg-10 layer-center">
+					<div class="exercise-screen">
+						<!-- <ExerciseScreen
+            :card="card"
+            :suffled-deck="suffledDeck"
+            :is-mobile="settings.isMobile"
+          /> -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script lang="ts" setup>
-
+	const title = 'A PRÁTICA'
+	const toast = useTt('Aguardando início', '', 3000)
 </script>
 
-
-
-<style>
-
-</style>
+<style></style>
