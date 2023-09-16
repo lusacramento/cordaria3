c88120dd5999014cbb316b4af5655d456c00ca90
lusacramento <lucianossacramento@yahoo.com.br>
2023-09-16 17:38:47 -0300

    feat implements cards view

--- pages/a-pratica/index.vue
+++ pages/a-pratica/index.vue
@@ -2,26 +2,19 @@
 	<div id="the-pratice" class="cordaria">
 		<CordariaMenu />
 		<div class="exercise-nav container-fluid">
-			<div class="row text-center layer-top align-items-center">
+			<div
+				class="row text-center layer-top justify-content-center align-items-center"
+			>
 				<div class="col-12 col-lg-4">
 					<h1 class="title mt-3 mb-4">{{ title }}</h1>
 					<!-- :visible="isEnabledMenu" -->
-					<div class="col-12 col-lg-10">
-						<div
-							class="exercises-nav-layer d-flex flex-column align-items-center"
-						></div>
-					</div>
 				</div>
 			</div>
 			<div class="row justify-content-center bg-exercise-screen">
 				<div class="col-lg-10 layer-center">
-					<div class="exercise-screen">
-						<!-- <ExerciseScreen
-            :card="card"
-            :suffled-deck="suffledDeck"
-            :is-mobile="settings.isMobile"
-          /> -->
-					</div>
+					<!-- <div class="exercise-screen"> -->
+					<CordariaScreen />
+					<!-- </div> -->
 				</div>
 			</div>
 		</div>
