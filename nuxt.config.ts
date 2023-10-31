16532a1cff750907f40476465827a33da0687193
lusacramento <lucianossacramento@yahoo.com.br>
2023-10-31 17:39:56 -0300

    config debug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
+	devtools: { enabled: false },
+
 	devServer: {
 		port: 8000,
 	},
+
+	sourcemap: true,
+
+	debug: true,
+
 	css: ['~/assets/styles/main.scss'],
+
+	imports: {
+		dirs: ['composables/**'],
+	},
 })
