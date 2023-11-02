e7fc931825ae2a72a99a964d0233aeae203f1cee
lusacramento <lucianossacramento@yahoo.com.br>
2023-11-02 10:42:13 -0300

    feat animate cards mode

--- components/cordaria/Screen.vue
+++ components/cordaria/Screen.vue
@@ -2,38 +2,42 @@
 	<!-- <div class="exercise-screen layer-center mt-2 mb-4"> -->
 	<div class="container-fluid mt-3">
 		<CordariaScreenCounter />
-		<div v-if="isStart">
-			<div v-if="viewMode === '3Cards'">
-				<div class="row mt-4 d-flex align-items-center justify-content-around">
-					<CordariaScreenCard
-						id="prev-card"
-						:key="card.prev.id"
-						:card="card.prev"
-					/>
-					<CordariaScreenCard
-						id="current-card"
-						:key="card.current.id"
-						:card="card.current"
-					/>
-					<CordariaScreenCard
-						id="next-card"
-						:key="card.next.id"
-						:card="card.next"
-					/>
+		<Transition name="flip" mode="in-out">
+			<div v-if="isStart">
+				<div v-if="viewMode === '3Cards'">
+					<div
+						class="row mt-4 d-flex align-items-center justify-content-around"
+					>
+						<CordariaScreenCard
+							id="prev-card"
+							:key="card.prev.id"
+							:card="card.prev"
+						/>
+						<CordariaScreenCard
+							id="current-card"
+							:key="card.current.id"
+							:card="card.current"
+						/>
+						<CordariaScreenCard
+							id="next-card"
+							:key="card.next.id"
+							:card="card.next"
+						/>
+					</div>
+				</div>
+				<div v-else>
+					<div class="row justify-content-center align-items-center">
+						<CordariaScreenCard
+							v-for="card in deck"
+							:id="card.id"
+							:key="card.id"
+							:card="card"
+							class="card cardView"
+						/>
+					</div>
 				</div>
 			</div>
-			<div v-else>
-				<div class="row justify-content-center align-items-center">
-					<CordariaScreenCard
-						v-for="card in deck"
-						:id="card.id"
-						:key="card.id"
-						:card="card"
-						class="card cardView"
-					/>
-				</div>
-			</div>
-		</div>
+		</Transition>
 		<CordariaScreenCounter />
 	</div>
 	<!-- </div> -->
@@ -51,5 +55,20 @@
 		.exercise-screen {
 			height: 60vh;
 		}
+	}
+
+	.flip-enter-active {
+		transition: all 0.3s;
+		transform: rotateY(1deg);
+	}
+
+	.flip-leave-active {
+		transition: all 0.3s;
+	}
+
+	.flip-enter-from,
+	.flip-leave-to {
+		transform: rotateY(90deg);
+		opacity: 0;
 	}
 </style>
