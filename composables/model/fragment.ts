f99116a5ca89e62aace5191be14c85839389efe1
lusacramento <lucianossacramento@yahoo.com.br>
2024-04-11 17:14:22 -0300

    refactor fragment composable to class

--- composables/model/fragment.ts
+++ composables/model/fragment.ts
@@ -1,18 +1,17 @@
-export const useFragment = (
-	idCard?: string,
-	fragmentValue?: string,
-	isHighLitgh?: boolean,
-) => {
-	class Fragment {
-		id: string
-		value: string
-		isHighlight: boolean
+export class Fragment {
+	id: string
+	value: string
+	isHighlight: boolean
 
-		constructor(id: string, value: string) {
-			this.id = id
-			this.value = value
-			this.isHighlight = false
-		}
+	constructor(id: string, value: string) {
+		this.id = id
+		this.value = value
+		this.isHighlight = false
+	}
+
+	setIsHighlight(isHighlight: boolean) {
+		this.isHighlight = isHighlight
+	}
 
 	getEmptyFragment() {
 		return new Fragment('null', 'null')
