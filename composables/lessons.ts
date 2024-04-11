c66e5044a5f037374d370bf2b30a3481bf1b5dbd
lusacramento <lucianossacramento@yahoo.com.br>
2024-04-11 17:26:52 -0300

    implements get lesson function

--- composables/lessons.ts
+++ composables/lessons.ts
@@ -1,3 +1,8 @@
+	function getLesson(id: number) {
+		const lesson = lessons.find((lesson) => lesson.id === id)
+		return lesson ? lesson : getEmptyLesson()
+	}
+
 	function getEmptyLesson() {
 		return { id: 0, name: '', firstFinger: 0, stringNumber: '', bpm: 0 }
 	}
@@ -7,6 +12,7 @@ const getLessons = () => lessons
 export const useLessons = () => {
 	return {
 		getLessons,
+		getLesson,
 		getEmptyLesson,
 	}
 }
