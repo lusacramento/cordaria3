5fb38d9a3e8fe18f4fd7939b4752c8ff4ac3236a
lusacramento <lucianossacramento@yahoo.com.br>
2024-05-30 15:38:31 -0300

    feat user sign-up form

---
+++ components/user/SignUp.vue
@@ -0,0 +1,108 @@
+<template>
+	<VeeForm
+		id="form-login"
+		class="container"
+		:validation-schema="schema"
+		@submit="signUp"
+	>
+		<div class="mb-3">
+			<div class="row d-flex align-items-center">
+				<div class="col-4 d-flex justify-self-start">
+					<label for="user" class="form-label">Nome de Usuário</label>
+				</div>
+				<div class="col-8">
+					<VeeField
+						id="user"
+						type="text"
+						class="form-control"
+						name="user"
+						required
+					/>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-auto">
+					<VeeErrorMessage name="user" class="text-danger" />
+				</div>
+			</div>
+		</div>
+		<div class="mb-3">
+			<div class="row d-flex align-items-center">
+				<div class="col-4 d-flex justify-self-start">
+					<label for="email" class="form-label">Email</label>
+				</div>
+				<div class="col-8">
+					<VeeField
+						id="email"
+						type="email"
+						class="form-control"
+						name="email"
+						required
+					/>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-auto">
+					<VeeErrorMessage name="email" class="text-danger" />
+				</div>
+			</div>
+		</div>
+		<div class="mb-3">
+			<div class="row d-flex align-items-center">
+				<div class="col-4 d-flex justify-self-start">
+					<label for="password" class="form-label">Senha</label>
+				</div>
+				<div class="col-8">
+					<VeeField
+						id="password"
+						type="password"
+						class="form-control"
+						name="password"
+						required
+					/>
+				</div>
+				<VeeErrorMessage name="password" class="text-danger" />
+			</div>
+		</div>
+		<div class="mb-3">
+			<div class="row d-flex align-items-center">
+				<div class="col-4 d-flex justify-self-start">
+					<label for="confirmPassword" class="form-label">Confirma Senha</label>
+				</div>
+				<div class="col-8">
+					<VeeField
+						id="confirmPassword"
+						type="password"
+						class="form-control"
+						name="confirmPassword"
+						required
+					/>
+				</div>
+				<VeeErrorMessage name="confirmPassword" class="text-danger" />
+			</div>
+		</div>
+
+		<button type="submit" class="btn btn-primary">Entrar</button>
+	</VeeForm>
+</template>
+
+<script lang="ts" setup>
+	import {
+		Form as VeeForm,
+		Field as VeeField,
+		ErrorMessage as VeeErrorMessage,
+	} from 'vee-validate'
+
+	const schema = {
+		user: 'required|min:3|max:16|alpha-num',
+		email: 'required|min:3|max:100|email',
+		password: 'required|min:9|max:100',
+		confirmPassword: 'passwords_mismatch:@password',
+	}
+
+	function signUp(values: any) {
+		console.log(values)
+	}
+</script>
+
+<style scoped></style>
