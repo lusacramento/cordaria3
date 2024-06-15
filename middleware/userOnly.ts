595c8e7120333e6b4e265a90e644864145ad1654
lusacramento <lucianossacramento@yahoo.com.br>
2024-06-15 16:40:01 -0300

    clear remove unused files

--- middleware/userOnly.ts
+++
@@ -1,7 +0,0 @@
-import { useAuthUser } from '~/composables/authUser'
-
-export default defineNuxtRouteMiddleware((to, from) => {
-	const user = useAuthUser()
-
-	if (!user.value) return navigateTo({ name: 'index' })
-})
