4a247270bd2f616e8179276f338a55eb8e7f108b
lusacramento <lucianossacramento@yahoo.com.br>
2024-06-15 16:44:35 -0300

    feat add user fields on store

--- stores/user.ts
+++ stores/user.ts
@@ -3,12 +3,18 @@ import { defineStore } from 'pinia'
 export const useMyUserStore = defineStore({
 	id: 'myUserStore',
 	state: () => ({
+		id: '',
 		userName: '',
+		email: '',
 		loggedIn: false,
 		theme: 'dark',
 	}),
 
 	getters: {
+		getId(state) {
+			return state.id
+		},
+
 		getNameUser(state) {
 			return state.userName
 		},
@@ -16,5 +22,23 @@ export const useMyUserStore = defineStore({
 			return state.loggedIn
 		},
 	},
-	actions: {},
+	actions: {
+		setId(id: string) {
+			this.id = id
+		},
+
+		setUserName(userName: string) {
+			this.userName = userName
+			return
+		},
+
+		logIn() {
+			this.loggedIn = true
+			return this.loggedIn
+		},
+
+		logOut() {
+			this.loggedIn = false
+		},
+	},
 })
