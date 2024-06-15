4559c6f24ffb6da414ff8c20a10dd02ad1945a3f
lusacramento <lucianossacramento@yahoo.com.br>
2024-06-15 16:43:41 -0300

    create rescue password blank page

---
+++ pages/recuperar-senha/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<div class="container justify-content-center mt-5">
+			<div class="collapse py-2 mx-5" id="collapseTarget">
+				This is the toggle-able content!
+			</div>
+			<button
+				class="btn btn-primary"
+				data-bs-target="#collapseTarget"
+				data-bs-toggle="collapse"
+			>
+				Bootstrap collapse
+			</button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
