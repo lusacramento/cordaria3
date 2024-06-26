3f0fc0489afbb768d8b3625b83fdaa4528d40364
lusacramento <lucianossacramento@yahoo.com.br>
2024-06-26 13:23:36 -0300

    implements user get route

---
+++ server/api/userdetails/index.get.ts
@@ -0,0 +1,25 @@
+import { UserDetails } from '~/server/models/UserDetails'
+export default defineEventHandler(async (event) => {
+	const { id } = getQuery(event)
+	if (!id)
+		throw createError({
+			statusCode: 400,
+			statusMessage: 'Bad Request',
+			message: 'O campo Id está vazio.',
+		})
+
+	try {
+		const response = await UserDetails.findOne({ userId: id })
+		if (!response)
+			throw createError({
+				statusCode: 404,
+				statusMessage: 'Not Found',
+				message: `Id ${{ id }} não encontrado!`,
+			})
+
+		return response
+	} catch (error) {
+		console.log(error)
+		return error
+	}
+})
