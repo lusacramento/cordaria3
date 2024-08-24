72bc88e595e9798c9aa50434f4097c0e4c86d62b
lusacramento <lucianossacramento@yahoo.com.br>
2024-08-24 16:21:08 -0300

    feat implements score api

---
+++ server/api/scores/index.get.ts
@@ -0,0 +1,22 @@
+import { Score } from '~/server/models/score'
+export default defineEventHandler(async (event) => {
+	const query = await getQuery(event)
+
+	if (!query.userId || !query.instrument)
+		throw createError({
+			statusCode: 400,
+			statusMessage: 'Bad Request',
+			message: 'Campos obrigatórios ausentes',
+		})
+
+	const score = await Score.findOne({ ...query })
+
+	if (!score)
+		throw createError({
+			statusCode: 404,
+			statusMessage: 'Not Found',
+			message: 'Dados não encontrados',
+		})
+
+	return score
+})
---
+++ server/api/scores/index.post.ts
@@ -0,0 +1,29 @@
+import { Score } from '~/server/models/score'
+import { Score as ScoreType } from '~/types/Score'
+
+export default defineEventHandler(async (event) => {
+	const body = (await readBody(event)) as ScoreType
+
+	try {
+		if (!body.userId || !body.instrument || !body.score) {
+			throw createError({
+				statusCode: 400,
+				statusMessage: 'Bad Request',
+				message: 'Campos obrigatórios ausentes',
+			})
+		}
+
+		const score = await Score.findOneAndUpdate(
+			{
+				userId: body.userId,
+				instrument: body.instrument,
+			},
+			{ ...body },
+			{ upsert: true, returnDocument: 'after' },
+		)
+
+		return { ...score?.toObject() }
+	} catch (error) {
+		return error
+	}
+})
