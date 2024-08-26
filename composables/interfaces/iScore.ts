cfbe1ab2888ba4036c5ed7128863e0f665d92a58
lusacramento <lucianossacramento@yahoo.com.br>
2024-08-26 16:06:53 -0300

    feat implements score interface

---
+++ composables/interfaces/iScore.ts
@@ -0,0 +1,20 @@
+import type { Score } from '~/types/Score'
+
+const url = '/api/score'
+
+async function getScore(userId: string, instrument: string) {
+	return $fetch(url, {
+		method: 'get',
+		query: { userId, instrument },
+	})
+}
+
+async function postScore(score: Score) {
+	return $fetch(url, {
+		body: score,
+		method: 'post',
+	})
+}
+export const useIScore = () => {
+	return { getScore, postScore }
+}
