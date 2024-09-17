1a74b422034eba168326a476b52457f6bffd8595
lusacramento <lucianossacramento@yahoo.com.br>
2024-09-17 20:21:27 -0300

    refactor script js to ts

--- pages/index.vue
+++ pages/index.vue
@@ -19,55 +19,41 @@
 	</div>
 </template>
 
-<script>
-	import Box from '@/components/box/Box.vue'
-
-	export default {
-		components: { Box },
-
-		data() {
-			return {
-				title: 'O PROJETO',
-				boxes: {
-					
-					callInAction: {
-						text: '<main><a style="font-size:1.5em" class="no-text-decoration" href="/a-pratica">Inicie agora seu treinamento!</a></main>',
-						schema: 'the-project',
-						leftLogo: true,
-						rightLogo: false,
-					},
-				},
-			}
-		},
-		head() {
-			return {
-				title: 'Cordaria - O Projeto',
-				meta: [
-					{
-						hid: 'titleprojeto',
-						name: 'title',
-						content: 'Cordaria - Já praticou hoje?',
-					},
-					{
-						hid: 'projeto',
-						name: 'description',
-						content:
-							'O projeto Cordaria é resultado de uma pesquisa que vem para auxiliar, educandos e professores, a prática de violão e guitarra',
-					},
-					{
-						hid: 'projetokeys',
-						name: 'keywords',
-						content:
-							'pesquisa, prática, música, projeto, violão, guitarra, método',
-					},
-				],
-				link: [
-					{
-						rel: 'canonical',
-						href: 'https://cordaria.com.br',
-					},
-				],
-			}
+<script lang="ts" setup>
+	useHead({
+		title: 'Cordaria - O Projeto',
+		meta: [
+			{
+				hid: 'titleprojeto',
+				name: 'title',
+				content: 'Cordaria - Já praticou hoje?',
+			},
+			{
+				hid: 'projeto',
+				name: 'description',
+				content:
+					'O projeto Cordaria é resultado de uma pesquisa que vem para auxiliar, educandos e professores, a prática de violão e guitarra',
+			},
+			{
+				hid: 'projetokeys',
+				name: 'keywords',
+				content: 'pesquisa, prática, música, projeto, violão, guitarra, método',
+			},
+		],
+		link: [
+			{
+				rel: 'canonical',
+				href: 'https://cordaria.com.br',
+			},
+		],
+	})
+	const title = 'O PROJETO'
+	const boxes = {
+		callInAction: {
+			text: '<main><a style="font-size:1.5em" class="no-text-decoration" href="/a-pratica">Inicie agora seu treinamento!</a></main>',
+			schema: 'the-project',
+			leftLogo: true,
+			rightLogo: false,
 		},
 	}
 </script>
