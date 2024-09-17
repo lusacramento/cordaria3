7de3061754ac6b883e3c8b0bd5bdc14a20732140
lusacramento <lucianossacramento@yahoo.com.br>
2024-09-17 13:20:28 -0300

    clean remove unused code

--- composables/controllers/db.ts
+++ composables/controllers/db.ts
@@ -1,26 +1,16 @@
 import type { ObjectId } from 'mongoose'
 import type { Instrument } from '~/types/Instrument'
-import type { Lesson } from '~/types/Lesson'
-import type { Progress } from '~/types/Progress'
-import type { Score } from '~/types/Score'
 import type { Settings } from '~/types/Settings.js'
 import { ViewMode } from '~/types/ViewMode'
 
 export const useDbController = () => {
 	const userStore = useMyUserStore()
 	const userDetailsStore = useMyUserDetailsStore()
-	const progressStore = useMyProgressStore()
 	const settingsStore = useMySettingsStore()
 	const iUser = useIUser()
-	const iLesson = useILesson()
-	const iProgress = useIProgress()
 	const iSettings = useISettings()
 
-	const iScore = useIScore()
-	const helpers = useHelpers()
-
 	// functions
-
 	// user
 	async function getUserDetails() {
 		return await iUser.getUserDetails(userStore.getId)
@@ -39,83 +29,6 @@ export const useDbController = () => {
 			userId: userDetailsStore.userId,
 			instrument: instrument,
 		})
-	}
-
-	// lesson
-	async function getLesson(number: number) {
-		const quantityOfStrings = helpers.getQuantityOfStrings(
-			settingsStore.getInstrument,
-		)
-
-		const lessonQuery = {
-			number: number,
-			quantityOfStrings: quantityOfStrings,
-		}
-		const response = await iLesson.getLesson(lessonQuery)
-
-		if (response?.lesson) {
-			const lesson = response.lesson as Lesson
-			progressStore.setLesson(lesson)
-			return lesson
-		}
-		return null
-	}
-
-	async function getLessonById(id: string) {
-		return await iLesson.getLessonById(id)
-	}
-	// progress
-	function generateProgress(lesson: Lesson) {
-		return {
-			userId: userStore.getId as unknown as ObjectId,
-			lesson: lesson._id as unknown as ObjectId,
-			isCompleted: false,
-			instrument: settingsStore.getInstrument,
-			currentLesson: lesson.number,
-		}
-	}
-
-	async function getProgress() {
-		return await iProgress.getProgress(
-			userStore.getId,
-			settingsStore.getInstrument,
-		)
-	}
-
-	async function postProgress(progress: Progress) {
-		return await iProgress.postProgress(progress)
-	}
-
-	async function updateProgress() {
-		await iProgress.setProgress(progressStore.getProgress)
-	}
-	// score
-	async function getScore() {
-		const score = (await iScore.getScore(
-			userStore.getId,
-			settingsStore.getInstrument,
-		)) as Score
-
-		return score.score as number
-	}
-
-	async function postScore() {
-		const score = {
-			userId: userStore.getId as unknown as ObjectId,
-			instrument: settingsStore.getInstrument,
-			score: progressStore.getScore,
-		} as unknown as Score
-		await iScore.postScore(score)
-	}
-
-	async function updateScore() {
-		const lessonPoints = progressStore.getCurrentLesson?.points
-		if (lessonPoints) {
-			const points = !progressStore.getProgress.isCompleted
-				? lessonPoints
-				: lessonPoints / 2
-			progressStore.setScore(points)
-		}
 	}
 
 	function createDefaultSettings(): Settings {
@@ -146,15 +59,6 @@ export const useDbController = () => {
 		getUserDetails,
 		postUserDetails,
 		updateUserDetails,
-		getLesson,
-		getLessonById,
-		generateProgress,
-		getProgress,
-		postProgress,
-		updateProgress,
-		getScore,
-		postScore,
-		updateScore,
 		createDefaultSettings,
 		getSettings,
 		postSettings,
