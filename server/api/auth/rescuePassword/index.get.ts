19c7af215c00b877eb2c01592c4ff06aa72e3416
lusacramento <lucianossacramento@yahoo.com.br>
2024-10-23 17:55:23 -0300

    clean remove unused file

--- server/api/auth/rescuePassword/index.get.ts
+++
@@ -1,8 +0,0 @@
-import { User } from '~/server/models/User'
-
-export default defineEventHandler(async (event) => {
-	const query = await getQuery(event)
-
-	const user = await User.findOne(query, { _id: 1, email: 1, username: 1 })
-	return user
-})
