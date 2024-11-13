768857e477cccd839b999f0b8118375097a715d2
lusacramento <lucianossacramento@yahoo.com.br>
2024-11-13 19:46:49 -0300

    fix access admin page

--- middleware/admin.ts
+++ middleware/admin.ts
@@ -1,12 +1,20 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
 	const { status, getSession } = useAuth()
 
-	const session = await getSession()
-	// @ts-ignore
-	const name = session.user.username
-	console.log(name)
+	console.log(status.value)
+	try {
+		const session = await getSession()
 
-	if (status.value !== 'authenticated' && name !== 'admin') {
+		console.log(session)
+
+		// @ts-ignore
+		const name = session.user.username
+		console.log(name)
+
+		if (status.value === 'authenticated' && name === 'admin') {
+			return
+		}
+	} catch (error) {
 		return navigateTo({
 			name: 'index',
 		})
