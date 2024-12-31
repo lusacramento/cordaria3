91cb54cce6a61f570c40b2cca38ccdd051f6286f
lusacramento <lucianossacramento@yahoo.com.br>
2024-12-30 22:00:38 -0300

    implements clean layout

---
+++ layouts/clean.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app" class="app container-fluid">
+    <div id="nav" class="d-flex">
+      <LayoutsNavClean />
+    </div>
+    <main id="main">
+      <slot />
+    </main>
+    <div id="footer container-fluid"></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+onMounted(() => {
+  inicializeStores()
+})
+
+function inicializeStores() {
+  useMyUserStore()
+  useMySettingsStore()
+}
+</script>
+
+<style scoped>
+@media (min-width: 991.98px) {
+  div.app {
+    grid-template-rows: 17vh 70vh 11vh;
+  }
+}
+
+div.app {
+  display: grid;
+  padding: 0;
+  margin: 0;
+
+  color: white;
+
+  grid-template-areas:
+    'nav'
+    'main'
+    'foot';
+}
+
+#nav {
+  grid-area: nav;
+}
+
+#main {
+  grid-area: main;
+  border-top: var(--border-main);
+}
+
+#footer {
+  grid-area: foot;
+}
+
+.hideFooter {
+  display: none;
+}
+
+.justify {
+  text-align: justify;
+}
+
+.no-text-decoration {
+  text-decoration: none;
+}
+</style>
