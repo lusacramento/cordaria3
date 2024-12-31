6aa8f9dae2fbac3cef318fc1b7f8a06fcbab3f2c
lusacramento <lucianossacramento@yahoo.com.br>
2024-12-31 13:25:56 -0300

    feat implements popup light theme

--- components/cordaria/Tips.vue
+++ components/cordaria/Tips.vue
@@ -3,155 +3,161 @@
 </template>
 
 <script lang="ts" setup>
-	import Swal from 'sweetalert2'
-	import type { LessonMessage } from '~/types/LessonMessage'
-	import AwardIcon1 from '~/assets/imgs/awards/big/award-1.png'
-	import AwardIcon2 from '~/assets/imgs/awards/big/award-2.png'
-	import AwardIcon3 from '~/assets/imgs/awards/big/award-3.png'
-	import AwardIcon4 from '~/assets/imgs/awards/big/award-4.png'
-	import AwardIcon5 from '~/assets/imgs/awards/big/award-5.png'
-	import AwardIcon6 from '~/assets/imgs/awards/big/award-6.png'
-	import AwardIcon7 from '~/assets/imgs/awards/big/award-7.png'
-	import AwardIcon8 from '~/assets/imgs/awards/big/award-8.png'
+import Swal from 'sweetalert2'
+import type { LessonMessage } from '~/types/LessonMessage'
+import AwardIcon1 from '~/assets/imgs/awards/big/award-1.png'
+import AwardIcon2 from '~/assets/imgs/awards/big/award-2.png'
+import AwardIcon3 from '~/assets/imgs/awards/big/award-3.png'
+import AwardIcon4 from '~/assets/imgs/awards/big/award-4.png'
+import AwardIcon5 from '~/assets/imgs/awards/big/award-5.png'
+import AwardIcon6 from '~/assets/imgs/awards/big/award-6.png'
+import AwardIcon7 from '~/assets/imgs/awards/big/award-7.png'
+import AwardIcon8 from '~/assets/imgs/awards/big/award-8.png'
 
-	import JSConfetti from 'js-confetti'
+import JSConfetti from 'js-confetti'
 
-	let timerEstimedInSeconds = 5
-	const timerEstimedInMiliSeconds = timerEstimedInSeconds * 1000
-	let timerInterval = 0
+const { value } = useColorMode()
 
-	const awardIcon = ref()
-	const iconHtml = ref()
-	const icon = ref()
+const bg = value === 'dark' ? '#000000b8' : '#e3e0d9e6'
 
-	const confetti = ref()
-	confetti.value = new JSConfetti()
+let timerEstimedInSeconds = 6000
+const timerEstimedInMiliSeconds = timerEstimedInSeconds * 1000
+let timerInterval = 0
 
-	async function showAlert(data: LessonMessage) {
-		if (data.isAwarded) {
-			confetti.value
-				.addConfetti({
-					confettiRadius: 3,
-					confettiNumber: 500,
-				})
-				.then(() => {
-					confetti.value.clearCanvas()
-				})
-		}
+const awardIcon = ref()
+const iconHtml = ref()
+const icon = ref()
 
-		if (data.isAwarded) {
-			switch (data.awardNumber) {
-				case 1:
-					awardIcon.value = AwardIcon1
-					break
-				case 2:
-					awardIcon.value = AwardIcon2
-					break
-				case 3:
-					awardIcon.value = AwardIcon3
-					break
-				case 4:
-					awardIcon.value = AwardIcon4
-					break
-				case 5:
-					awardIcon.value = AwardIcon5
-					break
-				case 6:
-					awardIcon.value = AwardIcon6
-					break
-				case 7:
-					awardIcon.value = AwardIcon7
-					break
-				case 8:
-					awardIcon.value = AwardIcon8
-					break
+const confetti = ref()
+confetti.value = new JSConfetti()
 
-				default:
-					awardIcon.value = null
-					icon.value = null
-					break
-			}
-
-			iconHtml.value = `<img src="${awardIcon.value}" alt="ícone do ${data.awardNumber}º troféu" class="big-award-icon d-flex justify-self-center"/>`
-		} else {
-			iconHtml.value = null
-			icon.value = data.icon
-		}
-
-		Swal.fire({
-			animation: false,
-			title: `<h1 class="title-tips">${data.title}</h1>`,
-			iconHtml: iconHtml.value,
-			icon: icon.value,
-			html: `<p class="text-tips">${data.description}</p>
-	        <p class="close-tips">Fechando em ${timerEstimedInSeconds} segundos.</p>`,
-			showCloseButton: true,
-			showConfirmButton: false,
-			timer: timerEstimedInMiliSeconds,
-			background: '#000000b8',
-			timerProgressBar: true,
-			didOpen: () => {
-				Swal.showLoading()
-				timerInterval = window.setInterval(() => {
-					const timerLeft = Swal.getTimerLeft()
-					if (typeof timerLeft == 'number') {
-						timerEstimedInSeconds = Math.round(timerLeft / 1000)
-					}
-
-					Swal.update({
-						html: `<p class="text-tips">${data.description}</p><p class="close-tips">Fechando em ${timerEstimedInSeconds} segundos.</p>`,
-					})
-				}, 200)
-			},
-			willClose: () => {
-				clearInterval(timerInterval)
-			},
-		})
+async function showAlert(data: LessonMessage) {
+	if (data.isAwarded) {
+		confetti.value
+			.addConfetti({
+				confettiRadius: 3,
+				confettiNumber: 500,
+			})
+			.then(() => {
+				confetti.value.clearCanvas()
+			})
 	}
 
-	defineExpose({ showAlert })
+	if (data.isAwarded) {
+		switch (data.awardNumber) {
+			case 1:
+				awardIcon.value = AwardIcon1
+				break
+			case 2:
+				awardIcon.value = AwardIcon2
+				break
+			case 3:
+				awardIcon.value = AwardIcon3
+				break
+			case 4:
+				awardIcon.value = AwardIcon4
+				break
+			case 5:
+				awardIcon.value = AwardIcon5
+				break
+			case 6:
+				awardIcon.value = AwardIcon6
+				break
+			case 7:
+				awardIcon.value = AwardIcon7
+				break
+			case 8:
+				awardIcon.value = AwardIcon8
+				break
+
+			default:
+				awardIcon.value = null
+				icon.value = null
+				break
+		}
+
+		iconHtml.value = `<img src="${awardIcon.value}" alt="ícone do ${data.awardNumber}º troféu" class="big-award-icon d-flex justify-self-center"/>`
+	} else {
+		iconHtml.value = null
+		icon.value = data.icon
+	}
+
+	Swal.fire({
+		animation: false,
+		title: `<h1 class="title-tips">${data.title}</h1>`,
+		iconHtml: iconHtml.value,
+		icon: icon.value,
+		html: `<p class="text-tips">${data.description}</p>
+	        <p class="close-tips">Fechando em ${timerEstimedInSeconds} segundos.</p>`,
+		showCloseButton: true,
+		showConfirmButton: false,
+		timer: timerEstimedInMiliSeconds,
+		background: bg,
+		timerProgressBar: true,
+		didOpen: () => {
+			Swal.showLoading()
+			timerInterval = window.setInterval(() => {
+				const timerLeft = Swal.getTimerLeft()
+				if (typeof timerLeft == 'number') {
+					timerEstimedInSeconds = Math.round(timerLeft / 1000)
+				}
+
+				Swal.update({
+					html: `<p class="text-tips">${data.description}</p><p class="close-tips">Fechando em ${timerEstimedInSeconds} segundos.</p>`,
+				})
+			}, 200)
+		},
+		willClose: () => {
+			clearInterval(timerInterval)
+		},
+	})
+}
+
+defineExpose({ showAlert })
 </script>
 
 <style>
-	h1.title-tips {
-		font-family: 'Encode Sans';
-		font-weight: var(--font-semi-bold);
-		color: var(--the-pratice-color);
-	}
-	p.text-tips {
-		font-family: 'Encode Sans';
-		font-weight: var(--font-regular);
-		color: rgba(128, 128, 128, 1);
-		text-align: center;
-		padding: 30px 0 0 0;
-		font-size: 1.2em;
-	}
-	p.close-tips {
-		font-family: 'Encode Sans';
-		font-weight: var(--font-light);
+h1.title-tips {
+	font-family: 'Encode Sans';
+	font-weight: var(--font-semi-bold);
+	color: var(--the-pratice-color);
+}
 
-		text-align: end;
-		padding: 40px 0 0 0;
-		font-size: 0.7em;
-	}
+p.text-tips {
+	font-family: 'Encode Sans';
+	font-weight: var(--font-regular);
+	color: rgba(128, 128, 128, 1);
+	text-align: center;
+	padding: 30px 0 0 0;
+	font-size: 1.2em;
+}
 
-	.swal2-select {
-		display: none !important;
-	}
+p.close-tips {
+	font-family: 'Encode Sans';
+	font-weight: var(--font-light);
 
-	.big-award-icon {
-		width: 130%;
-	}
+	text-align: end;
+	padding: 40px 0 0 0;
+	font-size: 0.7em;
+}
 
-	.swal2-icon,
-	.swal2-icon-content {
-		display: flex !important;
-		justify-content: center !important;
-		align-items: center !important;
-		border: none;
-	}
+.swal2-select {
+	display: none !important;
+}
 
-	canvas {
-		border: none;
-	}
+.big-award-icon {
+	width: 130%;
+}
+
+.swal2-icon,
+.swal2-icon-content {
+	display: flex !important;
+	justify-content: center !important;
+	align-items: center !important;
+	border: none;
+}
+
+canvas {
+	border: none;
+}
 </style>
