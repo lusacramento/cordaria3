1de2104045e6c33907ea563379b407a3258718e7
lusacramento <lucianossacramento@yahoo.com.br>
2025-01-21 14:49:53 -0300

    feat implements privacity terms

---
+++ components/PrivacityTerms.vue
@@ -0,0 +1,202 @@
+<template>
+	<div class="container privacity-terms mt-3">
+		<div class="container privacity mb-3 ">
+
+			<h2 class="mb-4 mt-3">🕵 Política Privacidade</h2>
+			<p>
+				A sua privacidade é importante para nós. É política do Cordaria App
+				respeitar a sua privacidade em relação a qualquer informação sua que possamos coletar no site <NuxtLink
+					to="/">Cordaria App</NuxtLink>, e outros
+				sites
+				que possuímos e operamos.
+			</p>
+			<p>
+				Solicitamos informações pessoais apenas quando realmente precisamos
+				delas para lhe fornecer um serviço. Fazemo-lo por meios justos e legais, com o seu conhecimento e
+				consentimento. Também informamos por que estamos coletando e como será usado.
+			</p>
+			<p>
+				Apenas retemos as informações coletadas pelo tempo necessário para
+				fornecer o serviço solicitado. Quando armazenamos dados, protegemos dentro de meios comercialmente
+				aceitáveis ​​para evitar perdas e roubos, bem como acesso, divulgação, cópia, uso ou modificação não
+				autorizados.
+			</p>
+			<p>
+				Não compartilhamos informações de identificação pessoal
+				publicamente ou com terceiros, exceto quando exigido por lei.
+			</p>
+			<p>
+				O nosso site pode ter links para sites externos que não são
+				operados por nós. Esteja ciente de que não temos controle sobre o conteúdo e práticas desses sites e
+				não podemos aceitar responsabilidade por suas respectivas <a href="https://politicaprivacidade.com/"
+					rel="noopener noreferrer" target="_blank">políticas de privacidade</a>.
+			</p>
+			<p>
+				Você é livre para recusar a nossa solicitação de informações
+				pessoais, entendendo que talvez não possamos fornecer alguns dos serviços desejados.
+			</p>
+			<p>
+				O uso continuado de nosso site será considerado como aceitação de
+				nossas práticas em torno de privacidade e informações pessoais. Se você tiver alguma dúvida sobre
+				como lidamos com dados do usuário e informações pessoais, entre em contacto connosco.
+			</p>
+			<ul>
+				<li>
+					O serviço Google AdSense que usamos para veicular
+					publicidade usa um cookie DoubleClick para veicular anúncios mais relevantes em toda a
+					Web e limitar o número de vezes que um determinado anúncio é exibido para você.
+				</li>
+				<li>
+					Para mais informações sobre o Google AdSense, consulte
+					as FAQs oficiais sobre privacidade do Google AdSense.
+				</li>
+				<li>
+					Utilizamos anúncios para compensar os custos de
+					funcionamento deste site e fornecer financiamento para futuros desenvolvimentos. Os
+					cookies de publicidade comportamental usados ​​por este site foram projetados para
+					garantir que você forneça os anúncios mais relevantes sempre que possível, rastreando
+					anonimamente seus interesses e apresentando coisas semelhantes que possam ser do seu
+					interesse.
+				</li>
+				<li>
+					Vários parceiros anunciam em nosso nome e os cookies
+					de rastreamento de afiliados simplesmente nos permitem ver se nossos clientes acessaram
+					o site através de um dos sites de nossos parceiros, para que possamos creditá-los
+					adequadamente e, quando aplicável, permitir que nossos parceiros afiliados ofereçam
+					qualquer promoção que pode fornecê-lo para fazer uma compra.
+				</li>
+			</ul>
+			<h3>Compromisso do Usuário</h3>
+			<p>
+				O usuário se compromete a fazer uso adequado dos conteúdos e da
+				informação que o Cordaria App oferece no site e com caráter enunciativo, mas não limitativo:
+			</p>
+			<ul>
+				<li>
+					Não se envolver em atividades que sejam ilegais ou
+					contrárias à boa fé a à ordem pública;
+				</li>
+				<li>
+					Não difundir propaganda ou conteúdo de natureza racista,
+					xenofóbica, "bets" ou azar, qualquer tipo de pornografia ilegal, de apologia ao
+					terrorismo ou contra os direitos humanos;
+				</li>
+				<li>
+					Não causar danos aos sistemas físicos (hardwares) e lógicos
+					(softwares) do Cordaria App, de seus fornecedores ou terceiros, para introduzir ou disseminar
+					vírus informáticos ou quaisquer outros sistemas de hardware ou software que sejam capazes de
+					causar danos anteriormente mencionados.
+				</li>
+			</ul>
+			<h3>Mais informações</h3>
+			<p>
+				Esperemos que esteja esclarecido e, como mencionado anteriormente,
+				se houver algo que você não tem certeza se precisa ou não, geralmente é mais seguro deixar os
+				cookies ativados, caso interaja com um dos recursos que você usa em nosso site.
+			</p>
+			<p>
+				Esta política é efetiva a partir de <strong>{{ privacityTermsDate }}</strong>.
+			</p>
+		</div>
+		<hr />
+		<div class="terms-and-conditions mb-3 mt-1">
+			<h2 class="mb-4">📜 Termos e Condições</h2>
+			<h3>1. Termos</h3>
+			<p>
+				Ao acessar ao site <NuxtLink to="/">Cordaria App</NuxtLink>,
+				concorda em cumprir estes termos de serviço, todas as leis e regulamentos aplicáveis ​​e concorda
+				que é responsável pelo cumprimento de todas as leis locais aplicáveis. Se você não concordar com
+				algum desses termos, está proibido de usar ou acessar este site. Os materiais contidos neste site
+				são protegidos pelas leis de direitos autorais e marcas comerciais aplicáveis.
+			</p>
+			<h3>2. Uso de Licença</h3>
+			<p>
+				É concedida permissão para baixar temporariamente uma cópia dos
+				materiais (informações ou software) no site Cordaria App , apenas para visualização transitória
+				pessoal e não comercial. Esta é a concessão de uma licença, não uma transferência de título e, sob
+				esta licença, você não pode:
+			</p>
+			<ul>
+				<li>modificar ou copiar os materiais;</li>
+				<li>usar os materiais para qualquer finalidade comercial ou para
+					exibição pública (comercial ou não comercial);</li>
+				<li>tentar descompilar ou fazer engenharia reversa de qualquer
+					software contido no site Cordaria App;</li>
+				<li>remover quaisquer direitos autorais ou outras notações de
+					propriedade dos materiais; ou</li>
+				<li>transferir os materiais para outra pessoa ou 'espelhe' os
+					materiais em qualquer outro servidor.</li>
+			</ul>
+			<p>
+				Esta licença será automaticamente rescindida se você violar alguma
+				dessas restrições e poderá ser rescindida por Cordaria App a qualquer momento. Ao encerrar a
+				visualização desses materiais ou após o término desta licença, você deve apagar todos os materiais
+				baixados em sua posse, seja em formato eletrónico ou impresso.
+			</p>
+			<h3>3. Isenção de responsabilidade</h3>
+			<ul>
+				<li>Os materiais no site da Cordaria App são fornecidos 'como
+					estão'. Cordaria App não oferece garantias, expressas ou implícitas, e, por este meio, isenta e
+					nega todas as outras garantias, incluindo, sem limitação, garantias implícitas ou condições de
+					comercialização, adequação a um fim específico ou não violação de propriedade intelectual ou
+					outra violação de direitos;</li>
+				<li>Além disso, o Cordaria App não garante ou faz qualquer
+					representação relativa à precisão, aos resultados prováveis ​​ou à confiabilidade do uso dos
+					materiais em seu site ou de outra forma relacionado a esses materiais ou em sites vinculados a
+					este site.</li>
+			</ul>
+			<h3>4. Limitações</h3>
+			<p>
+				Em nenhum caso o Cordaria App ou seus fornecedores serão
+				responsáveis ​​por quaisquer danos (incluindo, sem limitação, danos por perda de dados ou lucro ou
+				devido a interrupção dos negócios) decorrentes do uso ou da incapacidade de usar os materiais em
+				Cordaria App, mesmo que Cordaria App ou um representante autorizado da Cordaria App tenha sido
+				notificado oralmente ou por escrito da possibilidade de tais danos. Como algumas jurisdições não
+				permitem limitações em garantias implícitas, ou limitações de responsabilidade por danos
+				conseqüentes ou incidentais, essas limitações podem não se aplicar a você.
+			</p>
+			<h3>5. Precisão dos materiais</h3>
+			<p>
+				Os materiais exibidos no site da Cordaria App podem incluir erros
+				técnicos, tipográficos ou fotográficos. Cordaria App não garante que qualquer material em seu site
+				seja preciso, completo ou atual. Cordaria App pode fazer alterações nos materiais contidos em seu
+				site a qualquer momento, sem aviso prévio. No entanto, Cordaria App não se compromete a atualizar os
+				materiais.
+			</p>
+			<h3>6. Links</h3>
+			<p>O Cordaria App não analisou todos os sites vinculados ao seu site e
+				não é responsável pelo conteúdo de nenhum site vinculado. A inclusão de qualquer link não implica
+				endosso por Cordaria App do site. O uso de qualquer site vinculado é por conta e risco do
+				usuário.</p>
+			<h3>7. Modificações</h3>
+			<p>
+				O Cordaria App pode revisar estes termos de serviço do site a
+				qualquer momento, sem aviso prévio. Ao usar este site, você concorda em ficar vinculado à versão
+				atual desses termos de serviço.
+			</p>
+			<h3>8. Lei aplicável</h3>
+			<p>
+				Estes termos e condições são regidos e interpretados de acordo com
+				as leis do Cordaria App e você se submete irrevogavelmente à jurisdição exclusiva dos tribunais
+				naquele estado ou localidade e é efetiva a partir de <strong>{{ termsAndConditionsDate }}</strong>.
+			</p>
+
+		</div>
+	</div>
+	<br />
+
+</template>
+
+<script lang="ts" setup>
+const privacityTermsDate = '15 de janeiro de 2025 às 22h11'
+const termsAndConditionsDate = '15 de janeiro de 2025 às 22h11'
+
+</script>
+
+<style scoped>
+.privacity-terms {
+	text-align: justify;
+	border: solid 1px rgba(128, 128, 128, 0.3);
+	border-radius: 3px;
+}
+</style>
