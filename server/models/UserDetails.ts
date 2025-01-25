0d2c8c0a831fd28ffc4202e25024638066a6dcb8
lusacramento <lucianossacramento@yahoo.com.br>
2025-01-24 23:00:22 -0300

    fix document type import

--- server/models/UserDetails.ts
+++ server/models/UserDetails.ts
@@ -1,49 +1,49 @@
-import { model, ObjectId, Schema } from 'mongoose'
+import { Document, model, ObjectId, Schema } from "mongoose";
 
 export interface UserDetailsDocument extends Document {
-	userId: ObjectId
-	fullName: string
-	age: number
-	state: string
-	country: string
-	avatar: string
+  userId: ObjectId;
+  fullName: string;
+  age: number;
+  state: string;
+  country: string;
+  avatar: string;
 }
 
 const UserDetailsSchema = new Schema({
-	userId: {
-		type: Schema.ObjectId,
-		required: true,
-		unique: true,
-		index: true,
-	},
+  userId: {
+    type: Schema.ObjectId,
+    required: true,
+    unique: true,
+    index: true,
+  },
 
-	fullName: {
-		type: String,
-		required: true,
-	},
+  fullName: {
+    type: String,
+    required: true,
+  },
 
-	age: {
-		type: Number,
-		required: true,
-	},
+  age: {
+    type: Number,
+    required: true,
+  },
 
-	state: {
-		type: String,
-		required: true,
-	},
+  state: {
+    type: String,
+    required: true,
+  },
 
-	country: {
-		type: String,
-		required: true,
-	},
+  country: {
+    type: String,
+    required: true,
+  },
 
-	avatar: {
-		type: String,
-		required: true,
-	},
-})
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
 
 export const UserDetails = model<UserDetailsDocument>(
-	'UserDetails',
-	UserDetailsSchema,
-)
+  "UserDetails",
+  UserDetailsSchema
+);
--- server/models/progress.ts
+++ server/models/progress.ts
@@ -1,41 +1,41 @@
-import { model, ObjectId, Schema } from 'mongoose'
-import { Instrument } from '~/types/Instrument'
+import { Document, model, ObjectId, Schema } from "mongoose";
+import { Instrument } from "~/types/Instrument";
 
 export interface ProgressDocument extends Document {
-	userId: ObjectId
-	lesson: ObjectId
-	isCompleted: boolean
-	instrument: Instrument
-	currentLesson: number
+  userId: ObjectId;
+  lesson: ObjectId;
+  isCompleted: boolean;
+  instrument: Instrument;
+  currentLesson: number;
 }
 
 const ProgressSchema = new Schema({
-	userId: {
-		type: Schema.Types.ObjectId,
-		required: true,
-	},
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
 
-	lesson: {
-		type: Schema.Types.ObjectId,
-		required: true,
-	},
+  lesson: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
 
-	isCompleted: {
-		type: Boolean,
-		required: true,
-		default: false,
-	},
+  isCompleted: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
 
-	instrument: {
-		type: String,
-		enum: Object.values(Instrument),
-		required: true,
-	},
+  instrument: {
+    type: String,
+    enum: Object.values(Instrument),
+    required: true,
+  },
 
-	currentLesson: {
-		type: Number,
-		required: true,
-	},
-})
+  currentLesson: {
+    type: Number,
+    required: true,
+  },
+});
 
-export const Progress = model<ProgressDocument>('Progress', ProgressSchema)
+export const Progress = model<ProgressDocument>("Progress", ProgressSchema);
