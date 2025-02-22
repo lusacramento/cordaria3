b5a0809d04fad96eb6be82e054a7e29bd4b5fcb1
lusacramento <lucianossacramento@yahoo.com.br>
2025-02-22 13:36:23 -0300

    style formatter

--- composables/interfaces/iProgress.ts
+++ composables/interfaces/iProgress.ts
@@ -1,44 +1,44 @@
-import { Instrument } from './../../types/Instrument'
-import type { Progress } from '~/types/Progress'
+import { Instrument } from "./../../types/Instrument";
+import type { Progress } from "~/types/Progress";
 
 export const useIProgress = () => {
-	const url = '/api/progress'
-	const urlLastProgress = '/api/progress/last'
+  const url = "/api/progress";
+  const urlLastProgress = "/api/progress/last";
 
-	async function getProgress(
-		userId: string,
-		instrument: string,
-		currentLesson: number,
-	) {
-		return await $fetch(url, {
-			method: 'get',
-			params: {
-				userId,
-				instrument,
-				currentLesson,
-			},
-		})
-	}
+  async function getProgress(
+    userId: string,
+    instrument: string,
+    currentLesson: number
+  ) {
+    return await $fetch(url, {
+      method: "get",
+      params: {
+        userId,
+        instrument,
+        currentLesson,
+      },
+    });
+  }
 
-	async function getLastProgress(userId: string, instrument: Instrument) {
-		return await $fetch(urlLastProgress, {
-			method: 'get',
-			params: { userId, instrument },
-		})
-	}
+  async function getLastProgress(userId: string, instrument: Instrument) {
+    return await $fetch(urlLastProgress, {
+      method: "get",
+      params: { userId, instrument },
+    });
+  }
 
-	async function postProgress(progress: Progress) {
-		return await $fetch(url, {
-			method: 'post',
-			body: progress,
-		})
-	}
+  async function postProgress(progress: Progress) {
+    return await $fetch(url, {
+      method: "post",
+      body: progress,
+    });
+  }
 
-	async function setProgress(progress: Progress) {
-		return await $fetch(url, {
-			method: 'put',
-			body: progress,
-		})
-	}
-	return { getProgress, getLastProgress, postProgress, setProgress }
-}
+  async function setProgress(progress: any) {
+    return await $fetch(url, {
+      method: "put",
+      body: progress,
+    });
+  }
+  return { getProgress, getLastProgress, postProgress, setProgress };
+};
