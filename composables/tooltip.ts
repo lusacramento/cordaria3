7b0098bf185f24730984ef3c38c50bd8204708fd
lusacramento <lucianossacramento@yahoo.com.br>
2025-03-11 19:03:30 -0300

    feat documentate composables

--- composables/tooltip.ts
+++ composables/tooltip.ts
@@ -1,15 +1,26 @@
+/**
+ * A composable function that provides tooltip functionality using Bootstrap.
+ *
+ * @returns An object containing the `createTooltip` function.
+ */
 export const useTooltip = () => {
-	const bootstrap = () => import('bootstrap')
+  const bootstrap = () => import("bootstrap");
 
-	function createTooltip(el: Element) {
-		bootstrap().then((response) => {
-			return new response.Tooltip(el, {
-				fallbackPlacements: ['right'],
-				trigger: 'hover focus',
-			})
-		})
-	}
-	return {
-		createTooltip,
-	}
-}
+  /**
+   * Creates a Bootstrap tooltip for the given element.
+   *
+   * @param {Element} el - The DOM element to which the tooltip will be attached.
+   * @returns {Promise<void>} A promise that resolves when the tooltip is created.
+   */
+  function createTooltip(el: Element) {
+    bootstrap().then((response) => {
+      return new response.Tooltip(el, {
+        fallbackPlacements: ["right"],
+        trigger: "hover focus",
+      });
+    });
+  }
+  return {
+    createTooltip,
+  };
+};
--- composables/urls.ts
+++ composables/urls.ts
@@ -1,39 +1,53 @@
 export const useUrls = () => {
-	const urls = [
-		{
-			name: 'metronome',
-			baseUrl: '/audios/metronome/',
-		},
-		{
-			name: 'acoustic-guitar',
-			baseUrl: '/audios/acoustic-guitar/',
-		},
+  /**
+   * An array of objects representing different audio resources.
+   * Each object contains the name of the resource and its base URL.
+   *
+   * @type {Array<{name: string, baseUrl: string}>}
+   * @property {string} name - The name of the audio resource.
+   * @property {string} baseUrl - The base URL path to the audio resource.
+   */
+  const urls = [
+    {
+      name: "metronome",
+      baseUrl: "/audios/metronome/",
+    },
+    {
+      name: "acoustic-guitar",
+      baseUrl: "/audios/acoustic-guitar/",
+    },
 
-		{
-			name: 'electric-guitar',
-			baseUrl: '/audios/electric-guitar/',
-		},
-		{
-			name: 'bass',
-			baseUrl: '/audios/bass/',
-		},
-		{
-			name: 'cavaco',
-			baseUrl: '/audios/cavaco/',
-		},
-	]
+    {
+      name: "electric-guitar",
+      baseUrl: "/audios/electric-guitar/",
+    },
+    {
+      name: "bass",
+      baseUrl: "/audios/bass/",
+    },
+    {
+      name: "cavaco",
+      baseUrl: "/audios/cavaco/",
+    },
+  ];
 
-	const getUrl = function (instrumentName: string) {
-		let url: string = ''
-		urls.forEach((object) => {
-			if (instrumentName === object.name) url = object.baseUrl
-		})
+  /**
+   * Retrieves the base URL for a given instrument name.
+   *
+   * @param instrumentName - The name of the instrument to find the URL for.
+   * @returns The base URL of the instrument if found, otherwise returns 'Url não encontrada!'.
+   */
+  const getUrl = function (instrumentName: string) {
+    let url: string = "";
+    urls.forEach((object) => {
+      if (instrumentName === object.name) url = object.baseUrl;
+    });
 
-		if (url !== '') return url
-		else return 'Url não encontrada!'
-	}
+    if (url !== "") return url;
+    else return "Url não encontrada!";
+  };
 
-	return {
-		getUrl,
-	}
-}
+  return {
+    getUrl,
+  };
+};
